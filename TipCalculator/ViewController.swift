5dfa68f0323a395fb1f6436665a30ff73787cb50
dillonp23 <dillonp.lambda@gmail.com>
2019-07-19 19:03:26 -0700

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Dillon P on 7/19/19.
+//  Copyright © 2019 Dillon P. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var cookTimeLabel: UILabel!
+    @IBOutlet weak var weightTextField: UITextField!
+    @IBOutlet weak var timeToCookTextField: UITextField!
+    
+    // MARK: - Properites
+    
+    
+    
+    // MARK: - Functions
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+
+}
+
